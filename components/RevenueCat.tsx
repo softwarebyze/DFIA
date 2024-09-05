f5041dcd7184128d620a995f607e0b838f71abf3
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-09-04 22:19:31 -0400

    calling is working on ios

--- components/RevenueCat.tsx
+++ components/RevenueCat.tsx
@@ -1,36 +1,38 @@
 import { analytics } from "analytics";
 import { useEffect } from "react";
-import { Platform, Alert } from "react-native";
+import { Alert, Platform } from "react-native";
 import Purchases from "react-native-purchases";
 import RevenueCatUI from "react-native-purchases-ui";
 
 Purchases.setLogLevel(Purchases.LOG_LEVEL.VERBOSE);
 
+const configureRevenueCat = () => {
+  const apiKey = Platform.select({
+    // @ts-ignore
+    ios: process.env.EXPO_PUBLIC_RC_IOS!,
+    // @ts-ignore
+    android: process.env.EXPO_PUBLIC_RC_ANDROID!,
+  });
+
+  if (!apiKey) {
+    Alert.alert(
+      "Error configure RC",
+      "RevenueCat API key not provided"
+    );
+  } else {
+    Purchases.configure({ apiKey });
+  }
+};
+
 const useConfigureRevenueCat = () =>
   useEffect(() => {
-    if (Platform.OS === "ios") {
-      // @ts-ignore
-      const EXPO_PUBLIC_RC_IOS = process.env.EXPO_PUBLIC_RC_IOS!;
-      if (!EXPO_PUBLIC_RC_IOS) {
-        Alert.alert(
-          "Error configure RC",
-          "RevenueCat API key for ios not provided"
-        );
-      } else {
-        Purchases.configure({ apiKey: EXPO_PUBLIC_RC_IOS });
+    const setup = async () => {
+      if (!(await Purchases.isConfigured())) {
+        configureRevenueCat();
       }
-    } else if (Platform.OS === "android") {
-      // @ts-ignore
-      const EXPO_PUBLIC_RC_ANDROID = process.env.EXPO_PUBLIC_RC_ANDROID;
-      if (!EXPO_PUBLIC_RC_ANDROID) {
-        Alert.alert(
-          "Error configure RC",
-          "RevenueCat API key for android not provided"
-        );
-      } else {
-        Purchases.configure({ apiKey: EXPO_PUBLIC_RC_ANDROID });
-      }
-    }
+    };
+
+    setup();
   }, []);
 
 export const RevenueCat = ({ children }: { children: React.ReactNode }) => {
