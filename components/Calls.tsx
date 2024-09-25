ff5d1e6c3641ad044def5ca8996c7b35692d913e
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-09-25 00:18:02 -0400

    call card styling

--- components/Calls.tsx
+++ components/Calls.tsx
@@ -1,61 +1,38 @@
-import { Call, useStreamVideoClient } from "@stream-io/video-react-native-sdk";
+import {
+  StreamCall,
+  useCall,
+  useCalls,
+  useCallStateHooks,
+} from "@stream-io/video-react-native-sdk";
 import { Link } from "expo-router";
-import React, { useEffect, useState } from "react";
-import { Button, Pressable, Text, View } from "react-native";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
 
 export const Calls = () => {
-  const client = useStreamVideoClient();
-  if (!client) return;
-  const [renders, setRenders] = useState(0);
-
-  const { calls } = client?.state;
-
-  const calls2 = useOngoingCallsQuery();
+  const calls = useCalls();
 
   return (
     <View>
-      <Text style={{ fontSize: 8, color: "transparent" }}>{renders}</Text>
       <Text> {calls.length ? "All Calls" : "No calls"}</Text>
       {calls.map((call) => (
-        <CallCard key={call.id} call={call} />
+        <StreamCall key={call.id} call={call}>
+          <CallCard />
+        </StreamCall>
       ))}
-      <Text>{calls2?.length} Ongoing Call(s)</Text>
-      {calls2?.map((call) => (
-        <CallCard key={call.id} call={call} />
-      ))}
-      <Button title="Refresh" onPress={() => setRenders((r) => r + 1)} />
     </View>
   );
 };
 
-const useOngoingCallsQuery = () => {
-  const client = useStreamVideoClient();
-  if (!client) return;
-  const [calls, setCalls] = useState(client.state.calls);
+export const CallCard = () => {
+  const call = useCall();
+  const { useCallCallingState, useParticipants, useCallCreatedAt } =
+    useCallStateHooks();
 
-  useEffect(() => {
-    const fetchOnGoingCalls = async () => {
-      const { calls } = await client.queryCalls({
-        filter_conditions: {
-          ongoing: true,
+  const callingState = useCallCallingState();
+  const participants = useParticipants();
+  const createdAt = useCallCreatedAt();
 
-          $or: [
-            { created_by_user_id: client.streamClient.userID },
-            { members: { $in: [client.streamClient.userID] } },
-          ],
-        },
-        watch: true,
-      });
-      console.log("[fetchOngoingCalls] ", calls);
-      setCalls(calls);
-    };
-    fetchOnGoingCalls();
-  }, []);
-
-  return calls;
-};
-
-export const CallCard = ({ call }: { call: Call }) => {
+  if (!call) return;
   return (
     <Link asChild href={`/home/${call.id}`}>
       <Pressable
@@ -64,9 +41,20 @@ export const CallCard = ({ call }: { call: Call }) => {
           borderWidth: 2,
           borderColor: "gray",
           borderRadius: 8,
+          flexDirection: "row",
+          gap: 8
         }}
       >
-        <Text>{call.id}</Text>
+        <Text style={{ fontSize: 18 }}>📞</Text>
+        <View>
+          <Text>{callingState}</Text>
+          <Text>{participants.length} Participants</Text>
+          {participants.map((participant) => (
+            <Text key={participant.userId}>{participant.name}</Text>
+          ))}
+          <Text>{createdAt?.toLocaleTimeString()}</Text>
+          <Text style={{fontSize: 12}}>{call.id}</Text>
+        </View>
       </Pressable>
     </Link>
   );
