171e15363f6541213ae0c40f21ef725ecd722ba5
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-11-07 18:38:50 -0500

    added nw metro config

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+// eslint-disable-next-line no-undef
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+  inlineRem: 16,
+});
